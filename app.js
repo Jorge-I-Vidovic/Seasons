34296054acd6fa82535f7d89ef6772ac1dfee989
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-05-21 20:51:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ let resultado = document.getElementById('resultado');
 let boton = document.getElementById('boton');
 
 boton.addEventListener('click', function () {
-    let fecha = document.getElementById('fecha').value;
-    let fechaValue = new Date(fecha);
+    let fechaValue = document.getElementById('fecha').value;
+    let fecha = new Date(fechaValue);
 
-    let dia = fechaValue.getDay();
-    let mes = fechaValue.getMonth() + 1;
+    let dia = fecha.getDate();
+    let mes = fecha.getMonth() + 1;
     let estacion;
 
-    if (isNaN(fechaValue)) {
+    if (isNaN(fecha)) {
         resultado.innerHTML = "Escoje una fecha valida."
     } else {
-        if ((mes === 3 && dia >= 21) || mes >= 3 && mes <= 6 || (mes === 6 && dia < 21)) {
-            estacion = "Primavera";
-        } else if ((mes === 6 && dia >= 21) || mes >= 6 && mes <= 9 || (mes === 9 && dia < 21)) {
-            estacion = "Verano";
-        } else if ((mes === 9 && dia >= 21) || mes >= 9 && mes <= 12 || (mes === 12 && dia < 21)) {
-            estacion = "Otoño";
-        } else {
+        if ((mes === 12 && dia >= 21) || (mes >= 1 && mes < 3) || (mes === 3 && dia < 21)) {
             estacion = "Invierno";
+        } else if ((mes === 3 && dia >= 21) || (mes > 3 && mes < 6) || (mes === 6 && dia < 21)) {
+            estacion = "Primavera";
+        } else if ((mes === 6 && dia >= 21) || (mes > 6 && mes < 9) || (mes === 9 && dia < 21)) {
+            estacion = "Verano";
+        } else {
+            estacion = "Otoño";
         }
-        resultado.innerHTML = 'La estación del año correspondiente a ' + fechaValue.toLocaleDateString() + ' es ' + estacion;
+
+        resultado.innerHTML = 'La estación del año correspondiente a ' + fecha.toLocaleDateString() + ' es ' + estacion;
     }
 });
